6b40a1273278d8e3486266c1d7289d08ab3bb474
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-10-28 19:17:12 +0300

    15-3sum - Time Limit Exceeded solution

---
+++ 15-3sum.py
@@ -0,0 +1,44 @@
+"""
+Given an integer array nums, return all the triplets
+[nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+
+
+Constraints:
+
+0 <= nums.length <= 3000
+-10^5 <= nums[i] <= 10^5
+"""
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        triples = {tuple(sorted([nums[i], nums[j], nums[k]])) for k in range(n) for j in range(n) for i in range(n)
+                   if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0}
+        ans = [list(triple) for triple in triples]
+        return ans
+
+
+nums = [-1, 0, 1, 2, -1, -4]
+solution = Solution()
+print(solution.threeSum(nums))
+'''
+        triples = {tuple(sorted([nums[i], nums[j], nums[k]])) for k in range(n) for j in range(n) for i in range(n)
+        if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0}
+            ans = [list(triple) for triple in triples]
+'''
