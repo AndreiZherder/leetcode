b5456a639a2dd89287e8d17598df1bf2bb7e0bf0
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-10-28 21:26:31 +0300

    wrong answer

--- 15-3sum.py
+++ 15-3sum.py
@@ -27,14 +27,32 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        triples = {tuple(sorted([nums[i], nums[j], nums[k]])) for k in range(n) for j in range(n) for i in range(n)
-                   if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0}
-        ans = [list(triple) for triple in triples]
+        ans = []
+        pairs = dict()
+        singles = dict()
+        for num in nums:
+            if num not in pairs:
+                if num not in singles:
+                    singles[num] = 1
+                else:
+                    pairs[num] = 1
+                    del singles[num]
+        if pairs.get(0, 0) == 1 and not singles and len(nums) > 2:
+            ans.append([0, 0, 0])
+        for pair in pairs:
+            if -(pair + pair) in singles:
+                ans.append([pair, pair, -(pair + pair)])
+        for k in pairs:
+            singles[k] = 1
+        l = sorted(list(singles))
+        for i in range(len(l)):
+            for j in range(i + 1, len(l)):
+                if -(l[i] + l[j]) in singles or -(l[i] + l[j]) in pairs:
+                    if -(l[i] + l[j]) > l[j]:
+                        ans.append([l[i], l[j], -(l[i] + l[j])])
         return ans
 
-
-nums = [-1, 0, 1, 2, -1, -4]
+nums = [0, 0, 0]
 solution = Solution()
 print(solution.threeSum(nums))
 '''
