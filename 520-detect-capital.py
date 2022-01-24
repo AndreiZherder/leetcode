fd79aeef965e80a3d5c8e89abd0091fe534474d5
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-01-24 11:27:10 +0300

    520-detect-capital

---
+++ 520-detect-capital.py
@@ -0,0 +1,41 @@
+"""
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+
+
+Example 1:
+
+Input: word = "USA"
+Output: true
+Example 2:
+
+Input: word = "FlaG"
+Output: false
+
+
+Constraints:
+
+1 <= word.length <= 100
+word consists of lowercase and uppercase English letters.
+"""
+
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if ord('A') <= ord(word[-1]) <= ord('Z'):
+            return all(ord('A') <= ord(c) <= ord('Z') for c in word[len(word) - 2::-1])
+        else:
+            return all(ord('a') <= ord(c) <= ord('z') for c in word[len(word) - 2:0:-1])
+
+
+def main():
+    print(Solution().detectCapitalUse('u'))
+
+
+if __name__ == '__main__':
+    main()
