a27e46bb38f006a0d71587e026a49bd27987d510
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-03-28 01:01:05 +0300

    1337-the-k-weakest-rows-in-a-matrix (no debug print)

--- 1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix.py
@@ -73,7 +73,6 @@ class Solution:
 
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         strengths = [(self.bisect_right(row, 1), i) for i, row in enumerate(mat)]
-        print(strengths)
         return [i for strength, i in heapq.nsmallest(k, strengths)]
 
 
