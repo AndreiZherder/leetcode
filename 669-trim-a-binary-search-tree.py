23fb5059b8e0a87736a08a8b43aeb30a4829badd
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-04-15 11:30:36 +0300

    669-trim-a-binary-search-tree

---
+++ 669-trim-a-binary-search-tree.py
@@ -0,0 +1,126 @@
+"""
+Given the root of a binary search tree and the lowest and highest boundaries as low and high,
+trim the tree so that all its elements lies in [low, high].
+Trimming the tree should not change the relative structure of the elements that will remain in the tree
+(i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+
+Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+
+Example 1:
+Input: root = [1,0,2], low = 1, high = 2
+Output: [1,null,2]
+Example 2:
+Input: root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+Output: [3,2,null,1]
+
+
+Constraints:
+
+The number of nodes in the tree in the range [1, 10^4].
+0 <= Node.val <= 10^4
+The value of each node in the tree is unique.
+root is guaranteed to be a valid binary search tree.
+0 <= low <= high <= 10^4
+"""
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        def trim_low(node: TreeNode, parent: TreeNode, low: int):
+            if low <= node.val:
+                if node.left:
+                    trim_low(node.left, node, low)
+            else:
+                parent.left = node.right
+                if node.right:
+                    trim_low(node.right, parent, low)
+
+        def trim_high(node: TreeNode, parent: TreeNode, high: int):
+            if node.val <= high:
+                if node.right:
+                    trim_high(node.right, node, high)
+            else:
+                parent.right = node.left
+                if node.left:
+                    trim_high(node.left, parent, high)
+
+        dummy = TreeNode(0, root, None)
+        trim_low(root, dummy, low)
+        root = dummy.left
+        dummy = TreeNode(0, None, root)
+        trim_high(root, dummy, high)
+        root = dummy.right
+        return root
+
+
+def main():
+    vals = [3, 0, 4, None, 2, None, None, None, None, 1]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().trimBST(root, 1, 3))
+
+
+if __name__ == '__main__':
+    main()
