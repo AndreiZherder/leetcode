c4b4199ca19210ff0378b2017ae3189b5eaad5bc
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-04-17 11:19:34 +0300

    897-increasing-order-search-tree

---
+++ 897-increasing-order-search-tree.py
@@ -0,0 +1,105 @@
+"""
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree
+is now the root of the tree, and every node has no left child and only one right child.
+Example 1:
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+
+Constraints:
+
+The number of nodes in the given tree will be in the range [1, 100].
+0 <= Node.val <= 1000
+"""
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        dummy = TreeNode(0, None, None)
+        curr = dummy
+        for val in self.inorder(root):
+            curr.right = TreeNode(val)
+            curr = curr.right
+        return dummy.right
+
+    def inorder(self, root: TreeNode):
+        if not root:
+            return
+        yield from self.inorder(root.left)
+        yield root.val
+        yield from self.inorder(root.right)
+
+
+def main():
+    vals = [5, 3, 6, 2, 4, None, 8, 1, None, None, None, None, None, 7, 9]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().increasingBST(root))
+
+
+if __name__ == '__main__':
+    main()
