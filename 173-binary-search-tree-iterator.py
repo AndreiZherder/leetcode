f49c3718b2641214945cfec7ed5c3af7c9d453b8
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-04-20 10:49:16 +0300

    173-binary-search-tree-iterator

---
+++ 173-binary-search-tree-iterator.py
@@ -0,0 +1,152 @@
+"""
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class.
+The root of the BST is given as part of the constructor.
+The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer,
+otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number,
+the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid.
+That is, there will be at least a next number in the in-order traversal when next() is called.
+Example 1:
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 10^5].
+0 <= Node.val <= 10^6
+At most 10^5 calls will be made to hasNext, and next.
+
+
+Follow up:
+
+Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory,
+where h is the height of the tree?
+"""
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.nodes = self.inorder(root)
+        self.next_node = next(self.nodes)
+
+    def next(self) -> int:
+        val = self.next_node.val
+        try:
+            self.next_node = next(self.nodes)
+        except StopIteration:
+            self.next_node = None
+        return val
+
+    def hasNext(self) -> bool:
+        return bool(self.next_node)
+
+    def inorder(self, root: TreeNode):
+        if root:
+            yield from self.inorder(root.left)
+            yield root
+            yield from self.inorder(root.right)
+
+
+def main():
+    vals = [7, 3, 15, None, None, 9, 20]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    bSTIterator = BSTIterator(root)
+    print(bSTIterator.next())
+    print(bSTIterator.next())
+    print(bSTIterator.hasNext())
+    print(bSTIterator.next())
+    print(bSTIterator.hasNext())
+    print(bSTIterator.next())
+    print(bSTIterator.hasNext())
+    print(bSTIterator.next())
+    print(bSTIterator.hasNext())
+
+
+if __name__ == '__main__':
+    main()
