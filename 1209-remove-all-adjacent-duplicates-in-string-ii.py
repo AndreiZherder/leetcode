c685537f84a875cf9ae6e791a1aa02791aebe105
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-05-06 08:32:01 +0300

    1209-remove-all-adjacent-duplicates-in-string-ii

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,59 @@
+"""
+You are given a string s and an integer k, a k duplicate removal consists of choosing
+k adjacent and equal letters from s and removing them,
+causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation:
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+
+
+Constraints:
+
+1 <= s.length <= 10^5
+2 <= k <= 10^4
+s only contains lower case English letters.
+"""
+
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+        for ch in s:
+            if stack and stack[-1][0] == ch:
+                stack[-1][1] += 1
+            else:
+                stack.append([ch, 1])
+            if stack[-1][1] == k:
+                stack.pop()
+        return ''.join((pair[0] * pair[1] for pair in stack))
+
+
+def main():
+    s = "deeedbbcccbdaa"
+    k = 3
+    print(Solution().removeDuplicates(s, k))
+
+
+if __name__ == '__main__':
+    main()
