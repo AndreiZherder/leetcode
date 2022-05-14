7904cdd559d8dd93ebe00247c1c4324b3a1344ba
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-05-14 21:06:37 +0500

    743-network-delay-time

---
+++ 743-network-delay-time.py
@@ -0,0 +1,69 @@
+"""
+You are given a network of n nodes, labeled from 1 to n. You are also given times,
+a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node,
+and wi is the time it takes for a signal to travel from source to target.
+
+We will send a signal from a given node k. Return the time it takes for all the n nodes to receive the signal.
+If it is impossible for all the n nodes to receive the signal, return -1.
+
+
+
+Example 1:
+Input: times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+Output: 2
+Example 2:
+
+Input: times = [[1,2,1]], n = 2, k = 1
+Output: 1
+Example 3:
+
+Input: times = [[1,2,1]], n = 2, k = 2
+Output: -1
+
+
+Constraints:
+
+1 <= k <= n <= 100
+1 <= times.length <= 6000
+times[i].length == 3
+1 <= ui, vi <= n
+ui != vi
+0 <= wi <= 100
+All the pairs (ui, vi) are unique. (i.e., no multiple edges.)
+"""
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        nodes = [[] for i in range(n + 1)]
+        for u, v, w in times:
+            nodes[u].append((v, w))
+        queue = [(0, k)]
+        visited = set()
+        while queue:
+            dist, i = heappop(queue)
+            if i not in visited:
+                visited.add(i)
+                for v, w in nodes[i]:
+                    heappush(queue, (dist + w, v))
+                ans = dist
+        if len(visited) == len(nodes) - 1:
+            return ans
+        else:
+            return - 1
+
+
+def main():
+    # times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
+    # n = 4
+    # k = 2
+    times = [[1, 2, 1], [2, 1, 3]]
+    n = 2
+    k = 2
+    print(Solution().networkDelayTime(times, n, k))
+
+
+if __name__ == '__main__':
+    main()
