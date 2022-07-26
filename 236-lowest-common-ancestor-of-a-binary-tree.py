58dd3f942ad87ed87a26df3d7c1f76420e214733
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-07-26 12:04:39 +0300

    236-lowest-common-ancestor-of-a-binary-tree

---
+++ 236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,150 @@
+"""
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia:
+“The lowest common ancestor is defined between two nodes p and q as the lowest node in T
+that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 10^5].
+-10^9 <= Node.val <= 10^9
+All Node.val are unique.
+p != q
+p and q will exist in the tree.
+"""
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        path1 = [root]
+        path2 = [root]
+        def dfs(node: 'TreeNode') -> bool:
+            if node.left:
+                if path1[-1] != p:
+                    path1.append(node.left)
+                if path2[-1] != q:
+                    path2.append(node.left)
+                if path1[-1] == p and path2[-1] == q:
+                    return True
+                if dfs(node.left):
+                    return True
+                else:
+                    if path1[-1] != p:
+                        path1.pop()
+                    if path2[-1] != q:
+                        path2.pop()
+            if node.right:
+                if path1[-1] != p:
+                    path1.append(node.right)
+                if path2[-1] != q:
+                    path2.append(node.right)
+                if path1[-1] == p and path2[-1] == q:
+                    return True
+                if dfs(node.right):
+                    return True
+                else:
+                    if path1[-1] != p:
+                        path1.pop()
+                    if path2[-1] != q:
+                        path2.pop()
+            return False
+        dfs(root)
+        ans = path1[0]
+        for i in range(1, min(len(path1), len(path2))):
+            if path1[i] != path2[i]:
+                break
+            else:
+                ans = path1[i]
+        return ans
+
+
+def main():
+    vals = [3, 5, 1, 6, 2, 0, 8, None, None, 7, 4]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    p = nodes[9]
+    q = nodes[3]
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().lowestCommonAncestor(root, p, q))
+
+
+if __name__ == '__main__':
+    main()
