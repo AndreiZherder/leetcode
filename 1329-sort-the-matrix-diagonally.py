e38ef55ae561847a3236a78979c3f0219789343e
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-08-29 00:00:02 +0300

    1329-sort-the-matrix-diagonally

---
+++ 1329-sort-the-matrix-diagonally.py
@@ -0,0 +1,76 @@
+"""
+A matrix diagonal is a diagonal line of cells starting from some cell in either the topmost row or leftmost column
+and going in the bottom-right direction until reaching the matrix's end.
+For example, the matrix diagonal starting from mat[2][0], where mat is a 6 x 3 matrix,
+includes cells mat[2][0], mat[3][1], and mat[4][2].
+
+Given an m x n matrix mat of integers, sort each matrix diagonal in ascending order and return the resulting matrix.
+
+
+
+Example 1:
+Input: mat = [[3,3,1,1],[2,2,1,2],[1,1,1,2]]
+Output: [[1,1,1,1],[1,2,2,2],[1,2,3,3]]
+Example 2:
+
+Input: mat = [[11,25,66,1,69,7],[23,55,17,45,15,52],[75,31,36,44,58,8],[22,27,33,25,68,4],[84,28,14,11,5,50]]
+Output: [[5,17,4,1,52,7],[11,11,25,45,8,69],[14,23,25,44,58,15],[22,27,31,36,50,66],[84,28,75,33,55,68]]
+
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+1 <= mat[i][j] <= 100
+"""
+from heapq import heappush, heappop
+from typing import List
+
+
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        n = len(mat)
+        m = len(mat[0])
+        h = []
+        for col in range(m):
+            i = 0
+            j = col
+            while i < n and j < m:
+                heappush(h, mat[i][j])
+                i += 1
+                j += 1
+            i = 0
+            j = col
+            while i < n and j < m:
+                mat[i][j] = heappop(h)
+                i += 1
+                j += 1
+
+        for row in range(1, n):
+            i = row
+            j = 0
+            while i < n and j < m:
+                heappush(h, mat[i][j])
+                i += 1
+                j += 1
+            i = row
+            j = 0
+            while i < n and j < m:
+                mat[i][j] = heappop(h)
+                i += 1
+                j += 1
+        return mat
+
+
+def main():
+    mat = [[11, 25, 66, 1, 69, 7],
+           [23, 55, 17, 45, 15, 52],
+           [75, 31, 36, 44, 58, 8],
+           [22, 27, 33, 25, 68, 4],
+           [84, 28, 14, 11, 5, 50]]
+    print(Solution().diagonalSort(mat))
+
+
+if __name__ == '__main__':
+    main()
