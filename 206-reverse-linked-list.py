a8175aad5a76a6fddf5c4c08c2efea8bf832986f
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-08-29 10:24:44 +0300

    206-reverse-linked-list

---
+++ 206-reverse-linked-list.py
@@ -0,0 +1,69 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+"""
+from typing import Optional, List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        node = head
+        while node:
+            node.next, node, prev = prev, node.next, node
+        return prev
+
+
+def make_list(array: List[int]) -> Optional[ListNode]:
+    dummy = ListNode(0)
+    node = dummy
+    for num in array:
+        node.next = ListNode(num)
+        node = node.next
+    return dummy.next
+
+
+def print_list(head: ListNode):
+    node = head
+    while node:
+        print(node.val, end=' ')
+        node = node.next
+    print()
+
+
+def main():
+    list1 = [1, 2, 2, 2, 3, 3]
+    head = make_list(list1)
+    print_list(head)
+    print_list(Solution().reverseList(head))
+
+
+if __name__ == '__main__':
+    main()
