3d7d705b9eeba75c6a51a6b7626c6cf787df8fa7
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-10-09 12:06:29 +0300

    653-two-sum-iv-input-is-a-bst

---
+++ 653-two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,141 @@
+"""
+Given the root of a Binary Search Tree and a target number k,
+return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+
+
+Example 1:
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 10^4].
+-10^4 <= Node.val <= 10^4
+root is guaranteed to be a valid binary search tree.
+-10^5 <= k <= 10^5
+"""
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        def left_to_right(root: TreeNode):
+            stack = []
+            node = root
+            while node or stack:
+                if node:
+                    stack.append(node)
+                    node = node.left
+                else:
+                    node = stack.pop()
+                    yield node
+                    node = node.right
+
+        def right_to_left(root: TreeNode):
+            stack = []
+            node = root
+            while node or stack:
+                if node:
+                    stack.append(node)
+                    node = node.right
+                else:
+                    node = stack.pop()
+                    yield node
+                    node = node.left
+
+        nodes1 = left_to_right(root)
+        nodes2 = right_to_left(root)
+        left = next(nodes1)
+        right = next(nodes2)
+        if left == right:
+            return False
+        if left.val + right.val == k:
+            return True
+        while left != right:
+            if left.val + right.val == k:
+                return True
+            if left.val + right.val < k:
+                left = next(nodes1)
+            else:
+                right = next(nodes2)
+        return False
+
+
+def main():
+    vals = [5, 3, 6, 2, 4, None, 7]
+    k = 28
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+
+    print(Solution().findTarget(root, k))
+
+
+if __name__ == '__main__':
+    main()
