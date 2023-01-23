b3bb5ff9373c745185c724073f79a1fee57909be
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-01-23 17:37:25 +0300

    416-partition-equal-subset-sum

---
+++ 416-partition-equal-subset-sum.py
@@ -0,0 +1,62 @@
+"""
+Given an integer array nums, return true if you can partition the array into two subsets such that
+the sum of the elements in both subsets is equal or false otherwise.
+
+
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+"""
+from functools import lru_cache
+from typing import List
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        @lru_cache(None)
+        def dfs(i: int, target: int) -> bool:
+            if i == n:
+                if target == 0:
+                    return True
+                else:
+                    return False
+            return dfs(i + 1, target) or dfs(i + 1, target - nums[i])
+
+        n = len(nums)
+        target = sum(nums)
+        if target % 2 == 1:
+            return False
+        target //= 2
+        return dfs(0, target)
+
+
+def main():
+    nums = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 99, 97]
+    print(Solution().canPartition(nums))
+
+
+if __name__ == '__main__':
+    main()
