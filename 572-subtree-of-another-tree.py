0e3081ec63f6de57d75b9a2f0eeb6ee36e98952f
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-01-25 23:31:04 +0300

    572-subtree-of-another-tree

---
+++ 572-subtree-of-another-tree.py
@@ -0,0 +1,128 @@
+"""
+Given the roots of two binary trees root and subRoot,
+return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants.
+The tree tree could also be considered as a subtree of itself.
+
+
+
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+
+
+Constraints:
+
+The number of nodes in the root tree is in the range [1, 2000].
+The number of nodes in the subRoot tree is in the range [1, 1000].
+-10^4 <= root.val <= 10^4
+-10^4 <= subRoot.val <= 10^4
+"""
+from typing import Optional, Tuple, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def find(root: TreeNode, node: TreeNode) -> List[TreeNode]:
+            if not root:
+                return []
+            ans = []
+            if root.val == node.val:
+                ans.append(root)
+            ans.extend(find(root.left, node))
+            ans.extend(find(root.right, node))
+            return ans
+
+        def same(root1: TreeNode, root2: TreeNode) -> bool:
+            if not root1 and not root2:
+                return True
+            elif not root1 or not root2 or root1.val != root2.val:
+                return False
+            return same(root1.left, root2.left) and same(root1.right, root2.right)
+
+        return any(same(cur, subRoot) for cur in find(root, subRoot))
+
+
+def main():
+    vals = [3, 4, 5, 1, 2]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    vals = [4, 1, 2]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    subRoot = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(subRoot)
+
+    print(Solution().isSubtree(root, subRoot))
+
+
+if __name__ == '__main__':
+    main()
