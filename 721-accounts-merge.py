b6257d5f2e475eb6c377a272e8fe366d2fc0facb
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-02-08 18:55:59 +0300

    721-accounts-merge

---
+++ 721-accounts-merge.py
@@ -0,0 +1,115 @@
+"""
+Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0]
+is a name, and the rest of the elements are emails representing emails of the account.
+
+Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common
+email to both accounts. Note that even if two accounts have the same name,
+they may belong to different people as people could have the same name.
+A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+
+After merging the accounts, return the accounts in the following format: the first element of each account is the name,
+and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+
+
+Example 1:
+
+Input: accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com",
+"john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+Output: [["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],
+["John","johnnybravo@mail.com"]]
+Explanation:
+The first and second John's are the same person as they have the common email "johnsmith@mail.com".
+The third John and Mary are different people as none of their email addresses are used by other accounts.
+We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'],
+['John', 'johnnybravo@mail.com'],
+['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+Example 2:
+
+Input: accounts = [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],
+["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],
+["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+Output: [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],
+["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],
+["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+
+
+Constraints:
+
+1 <= accounts.length <= 1000
+2 <= accounts[i].length <= 10
+1 <= accounts[i][j].length <= 30
+accounts[i][0] consists of English letters.
+accounts[i][j] (for j > 0) is a valid email.
+"""
+from collections import defaultdict
+from typing import List
+
+
+class DSU:
+    def __init__(self, n: int):
+        self.n = n
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, i: int) -> int:
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i: int, j: int):
+        id1 = self.find(i)
+        id2 = self.find(j)
+        if id1 == id2:
+            return
+        if self.rank[id1] > self.rank[id2]:
+            self.parent[id2] = id1
+        else:
+            self.parent[id1] = id2
+            if self.rank[id1] == self.rank[id2]:
+                self.rank[id2] += 1
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        emails = []
+        i = 0
+        n = sum(len(account) for account in accounts) - len(accounts)
+        dsu = DSU(n)
+        seen = dict()
+        for account in accounts:
+            name = account[0]
+            for j, email in enumerate(account[1:], start=1):
+                emails.append((email, name))
+                if j > 1:
+                    dsu.union(i, i - 1)
+                if email in seen:
+                    dsu.union(i, seen[email])
+                else:
+                    seen[email] = i
+                i += 1
+
+        d = defaultdict(set)
+        names = dict()
+        for i, (email, name) in enumerate(emails):
+            key = dsu.find(i)
+            d[key].add(email)
+            names[key] = name
+        ans = []
+        for key in names:
+            ans.append([names[key]] + sorted(d[key]))
+        return ans
+
+
+def main():
+    accounts = [
+        ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+        ["John", "johnsmith@mail.com", "john00@mail.com"],
+        ["Mary", "mary@mail.com"],
+        ["John", "johnnybravo@mail.com"]
+    ]
+    print(Solution().accountsMerge(accounts))
+
+
+if __name__ == '__main__':
+    main()
