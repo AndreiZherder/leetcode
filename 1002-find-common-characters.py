42d1ba9775c296c15447cf4fbe8cf21010dce83e
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-02-23 16:36:01 +0300

    1002-find-common-characters

---
+++ 1002-find-common-characters.py
@@ -0,0 +1,39 @@
+"""
+Given a string array words, return an array of all characters that show up in all strings within the words
+(including duplicates). You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+"""
+from collections import Counter
+from functools import reduce
+from typing import List
+
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        return list(reduce(lambda x, y: x & y, (Counter(word) for word in words)).elements())
+
+
+def main():
+    words = ["bella","label","roller"]
+    print(Solution().commonChars(words))
+
+
+if __name__ == '__main__':
+    main()
