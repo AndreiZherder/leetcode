d92f46d8cc2d217330b99fa5be993f5bb0f37587
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-03-11 09:01:58 +0300

    109-convert-sorted-list-to-binary-search-tree

---
+++ 109-convert-sorted-list-to-binary-search-tree.py
@@ -0,0 +1,117 @@
+"""
+Given the head of a singly linked list where elements are sorted in ascending order, convert it to a
+height-balanced
+ binary search tree.
+
+
+
+Example 1:
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+Example 2:
+
+Input: head = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in head is in the range [0, 2 * 10^4].
+-10^5 <= Node.val <= 10^5
+"""
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        def build_tree(l: int, r: int) -> Optional[TreeNode]:
+            if l > r:
+                return None
+            if l == r:
+                return TreeNode(arr[l])
+            mid = l + (r - l) // 2
+            return TreeNode(arr[mid], build_tree(l, mid - 1), build_tree(mid + 1, r))
+
+        node = head
+        arr = []
+        while node:
+            arr.append(node.val)
+            node = node.next
+        return build_tree(0, len(arr) - 1)
+
+
+def main():
+    lst = [-10, -3, 0, 5, 9]
+    head = ListNode(lst[0]) if lst else None
+    node = head
+    for num in lst[1:]:
+        node.next = ListNode(num)
+        node = node.next
+    print(Solution().sortedListToBST(head))
+
+
+if __name__ == '__main__':
+    main()
