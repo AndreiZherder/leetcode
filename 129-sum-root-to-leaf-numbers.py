4c881e00f2c6633c4ab30e5190b2771ecaec50f1
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-03-14 10:38:30 +0300

    129-sum-root-to-leaf-numbers

---
+++ 129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,109 @@
+"""
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+
+
+Example 1:
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 10^4].
+-2^31 <= Node.val <= 2^31 - 1
+"""
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(cur: List[int], root: Optional[TreeNode]):
+            if not root.left and not root.right:
+                cur += [root.val]
+                nums.append(int(''.join(map(str, cur))))
+                return
+            if root.left:
+                dfs(cur + [root.val], root.left)
+            if root.right:
+                dfs(cur + [root.val], root.right)
+
+        nums = []
+        dfs([], root)
+        return sum(nums)
+
+
+def main():
+    vals = [4, 9, 0, 5, 1]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().sumNumbers(root))
+
+
+if __name__ == '__main__':
+    main()
