135fdf02a952ba9b0fee6cabf220380f3bd8364a
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-03-15 16:17:09 +0300

    868-binary-gap

---
+++ 868-binary-gap.py
@@ -0,0 +1,66 @@
+"""
+Given a positive integer n, find and return the longest distance between any two adjacent 1's
+in the binary representation of n. If there are no two adjacent 1's, return 0.
+
+Two 1's are adjacent if there are only 0's separating them (possibly no 0's).
+The distance between two 1's is the absolute difference between their bit positions.
+For example, the two 1's in "1001" have a distance of 3.
+
+
+
+Example 1:
+
+Input: n = 22
+Output: 2
+Explanation: 22 in binary is "10110".
+The first adjacent pair of 1's is "10110" with a distance of 2.
+The second adjacent pair of 1's is "10110" with a distance of 1.
+The answer is the largest of these two distances, which is 2.
+Note that "10110" is not a valid pair since there is a 1 separating the two 1's underlined.
+Example 2:
+
+Input: n = 8
+Output: 0
+Explanation: 8 in binary is "1000".
+There are not any adjacent pairs of 1's in the binary representation of 8, so we return 0.
+Example 3:
+
+Input: n = 5
+Output: 2
+Explanation: 5 in binary is "101".
+
+
+Constraints:
+
+1 <= n <= 10^9
+"""
+
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        def dec_to_bin(x: int, n: int) -> str:
+            return bin(x)[2:].zfill(n)
+
+        start = False
+        best = 0
+        cur = 0
+        for num in dec_to_bin(n, 32):
+            if num == '1':
+                if start:
+                    best = max(best, cur + 1)
+                start = True
+                cur = 0
+            else:
+                if start:
+                    cur += 1
+        return best
+
+
+
+def main():
+    n = 22
+    print(Solution().binaryGap(n))
+
+
+if __name__ == '__main__':
+    main()
