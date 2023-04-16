ebf754f7ae6f4cc1ab3d5d5fc75b456977d9b6be
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-04-16 12:03:07 +0300

    2641-cousins-in-binary-tree-ii

---
+++ 2641-cousins-in-binary-tree-ii.py
@@ -0,0 +1,146 @@
+"""
+Given the root of a binary tree, replace the value of each node in the tree with the sum of all its cousins' values.
+
+Two nodes of a binary tree are cousins if they have the same depth with different parents.
+
+Return the root of the modified tree.
+
+Note that the depth of a node is the number of edges in the path from the root node to it.
+
+
+
+Example 1:
+
+
+Input: root = [5,4,9,1,10,null,7]
+Output: [0,0,0,7,7,null,11]
+Explanation: The diagram above shows the initial binary tree and the binary tree after changing the value of each node.
+- Node with value 5 does not have any cousins so its sum is 0.
+- Node with value 4 does not have any cousins so its sum is 0.
+- Node with value 9 does not have any cousins so its sum is 0.
+- Node with value 1 has a cousin with value 7 so its sum is 7.
+- Node with value 10 has a cousin with value 7 so its sum is 7.
+- Node with value 7 has cousins with values 1 and 10 so its sum is 11.
+Example 2:
+
+
+Input: root = [3,1,2]
+Output: [0,0,0]
+Explanation: The diagram above shows the initial binary tree and the binary tree after changing the value of each node.
+- Node with value 3 does not have any cousins so its sum is 0.
+- Node with value 1 does not have any cousins so its sum is 0.
+- Node with value 2 does not have any cousins so its sum is 0.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 104
+"""
+from collections import deque, defaultdict
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs1(root: TreeNode, level: int):
+            levels[level] += root.val
+            if root.left:
+                dfs1(root.left, level + 1)
+            if root.right:
+                dfs1(root.right, level + 1)
+
+        def dfs2(root: TreeNode, level: int):
+            if root.left or root.right:
+                a = 0
+                b = 0
+                if root.left:
+                    a = root.left.val
+                if root.right:
+                    b = root.right.val
+                if root.left:
+                    root.left.val = levels[level + 1] - (a + b)
+                if root.right:
+                    root.right.val = levels[level + 1] - (a + b)
+            if root.left:
+                dfs2(root.left, level + 1)
+            if root.right:
+                dfs2(root.right, level + 1)
+
+        levels = defaultdict(int)
+        dfs1(root, 0)
+        dfs2(root, 0)
+        root.val = 0
+        return root
+
+
+def main():
+    vals = [5, 4, 9, 1, 10, None, 7]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().replaceValueInTree(root))
+
+
+if __name__ == '__main__':
+    main()
