fa17463a81f258e97f99af23ed6f44c687b43437
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-05-29 20:46:18 +0300

    762-prime-number-of-set-bits-in-binary-representation

---
+++ 762-prime-number-of-set-bits-in-binary-representation.py
@@ -0,0 +1,91 @@
+"""
+Given two integers left and right, return the count of numbers in the inclusive range [left, right]
+having a prime number of set bits in their binary representation.
+
+Recall that the number of set bits an integer has is the number of 1's present when written in binary.
+
+For example, 21 written in binary is 10101, which has 3 set bits.
+
+
+Example 1:
+
+Input: left = 6, right = 10
+Output: 4
+Explanation:
+6  -> 110 (2 set bits, 2 is prime)
+7  -> 111 (3 set bits, 3 is prime)
+8  -> 1000 (1 set bit, 1 is not prime)
+9  -> 1001 (2 set bits, 2 is prime)
+10 -> 1010 (2 set bits, 2 is prime)
+4 numbers have a prime number of set bits.
+Example 2:
+
+Input: left = 10, right = 15
+Output: 5
+Explanation:
+10 -> 1010 (2 set bits, 2 is prime)
+11 -> 1011 (3 set bits, 3 is prime)
+12 -> 1100 (2 set bits, 2 is prime)
+13 -> 1101 (3 set bits, 3 is prime)
+14 -> 1110 (3 set bits, 3 is prime)
+15 -> 1111 (4 set bits, 4 is not prime)
+5 numbers have a prime number of set bits.
+
+
+Constraints:
+
+1 <= left <= right <= 106
+0 <= right - left <= 104
+"""
+
+
+def is_prime(n: int):
+    if n == 2 or n == 3:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
+    # all primes > 3 are of the form 6n ± 1
+    p = 5
+    while p * p <= n:
+        if n % p == 0:
+            return False
+        if n % (p + 2) == 0:
+            return False
+        p += 6
+    return True
+
+
+def popcount(n: int) -> int:
+    """
+    Count set bits in an integer
+    """
+    c = (n & 0x5555555555555555) + ((n >> 1) & 0x5555555555555555)
+    c = (c & 0x3333333333333333) + ((c >> 2) & 0x3333333333333333)
+    c = (c & 0x0F0F0F0F0F0F0F0F) + ((c >> 4) & 0x0F0F0F0F0F0F0F0F)
+    c = (c & 0x00FF00FF00FF00FF) + ((c >> 8) & 0x00FF00FF00FF00FF)
+    c = (c & 0x0000FFFF0000FFFF) + ((c >> 16) & 0x0000FFFF0000FFFF)
+    c = (c & 0x00000000FFFFFFFF) + ((c >> 32) & 0x00000000FFFFFFFF)
+    return c
+
+
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        ans = 0
+        for i in range(left, right + 1):
+            if is_prime(popcount(i)):
+                ans += 1
+        return ans
+
+
+def main():
+    left = 10
+    right = 15
+    print(Solution().countPrimeSetBits(left, right))
+
+
+if __name__ == '__main__':
+    main()
