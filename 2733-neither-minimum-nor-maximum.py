878447509a194cf74be691dc5f23fcee60573ae2
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-06-14 15:03:12 +0300

    2733-neither-minimum-nor-maximum

---
+++ 2733-neither-minimum-nor-maximum.py
@@ -0,0 +1,58 @@
+"""
+Given an integer array nums containing distinct positive integers,
+find and return any number from the array that is neither the minimum nor the maximum value in the array,
+or -1 if there is no such number.
+
+Return the selected integer.
+
+
+
+Example 1:
+
+Input: nums = [3,2,1,4]
+Output: 2
+Explanation: In this example, the minimum value is 1 and the maximum value is 4.
+Therefore, either 2 or 3 can be valid answers.
+Example 2:
+
+Input: nums = [1,2]
+Output: -1
+Explanation: Since there is no number in nums that is neither the maximum nor the minimum,
+we cannot select a number that satisfies the given condition. Therefore, there is no answer.
+Example 3:
+
+Input: nums = [2,1,3]
+Output: 2
+Explanation: Since 2 is neither the maximum nor the minimum value in nums, it is the only valid answer.
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+All values in nums are distinct
+
+"""
+from typing import List
+
+
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        mn = 10 ** 20
+        mx = -10 ** 20
+        for x in nums:
+            mn = min(mn, x)
+            mx = max(mx, x)
+        for x in nums:
+            if x != mn and x != mx:
+                return x
+        return -1
+
+
+def main():
+    nums = [2, 1, 3]
+    print(Solution().findNonMinOrMax(nums))
+
+
+if __name__ == '__main__':
+    main()
