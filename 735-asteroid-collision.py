725c7243985292b874cd7f7b738ecd0163d52e1e
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-07-20 10:12:39 +0300

    735-asteroid-collision

---
+++ 735-asteroid-collision.py
@@ -0,0 +1,63 @@
+"""
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction
+(positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode.
+If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+
+
+Constraints:
+
+2 <= asteroids.length <= 104
+-1000 <= asteroids[i] <= 1000
+asteroids[i] != 0
+"""
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for num in asteroids:
+            while stack and stack[-1] > 0 and stack[-1] < -num:
+                stack.pop()
+            if not stack or stack[-1] < 0 or num > 0:
+                stack.append(num)
+                continue
+            if -num < stack[-1]:
+                continue
+            elif -num == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(num)
+        return stack
+
+
+
+def main():
+    asteroids = [5, 10, -5]
+    print(Solution().asteroidCollision(asteroids))
+
+
+if __name__ == '__main__':
+    main()
