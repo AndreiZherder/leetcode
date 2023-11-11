05b9daea0d1b03ef4c2b0631eb123d8714564168
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-11-11 19:12:19 +0300

    2929-distribute-candies-among-children-ii

---
+++ 2929-distribute-candies-among-children-ii.py
@@ -0,0 +1,52 @@
+"""
+You are given two positive integers n and limit.
+
+Return the total number of ways to distribute n candies among 3 children
+such that no child gets more than limit candies.
+
+
+
+Example 1:
+
+Input: n = 5, limit = 2
+Output: 3
+Explanation: There are 3 ways to distribute 5 candies such that no child gets more than 2 candies:
+(1, 2, 2), (2, 1, 2) and (2, 2, 1).
+Example 2:
+
+Input: n = 3, limit = 3
+Output: 10
+Explanation: There are 10 ways to distribute 3 candies such that no child gets more than 3 candies:
+(0, 0, 3), (0, 1, 2), (0, 2, 1), (0, 3, 0), (1, 0, 2), (1, 1, 1), (1, 2, 0), (2, 0, 1), (2, 1, 0) and (3, 0, 0).
+
+
+Constraints:
+
+1 <= n <= 106
+1 <= limit <= 106
+"""
+
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        if n > limit * 3:
+            return 0
+        ans = 0
+        for i in range(0, min(n, limit) + 1):
+            x = n - i
+            if x > limit * 2:
+                continue
+            if x <= limit:
+                ans += x + 1
+            else:
+                ans += limit - (x - limit) + 1
+        return ans
+
+
+def main():
+    n = 12
+    limit = 3
+    print(Solution().distributeCandies(n, limit))
+
+
+if __name__ == '__main__':
+    main()
