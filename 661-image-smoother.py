2c3b9595e763ad50e1024b1ab1499b3ce3a5b618
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-12-19 12:01:54 +0300

    661-image-smoother

---
+++ 661-image-smoother.py
@@ -0,0 +1,62 @@
+"""
+An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down
+the average of the cell and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother).
+If one or more of the surrounding cells of a cell is not present, we do not consider it in the average
+(i.e., the average of the four cells in the red smoother).
+Given an m x n integer matrix img representing the grayscale of an image,
+return the image after applying the smoother on each cell of it.
+
+
+
+Example 1:
+Input: img = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[0,0,0],[0,0,0],[0,0,0]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+For the points (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+Example 2:
+Input: img = [[100,200,100],[200,50,200],[100,200,100]]
+Output: [[137,141,137],[141,138,141],[137,141,137]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor((100+200+200+50)/4) = floor(137.5) = 137
+For the points (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = floor(141.666667) = 141
+For the point (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138
+
+
+Constraints:
+
+m == img.length
+n == img[i].length
+1 <= m, n <= 200
+0 <= img[i][j] <= 255
+
+"""
+from typing import List
+
+
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        d8 = ((0, 1), (-1, 0), (0, -1), (1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
+        n = len(img)
+        m = len(img[0])
+        ans = [[0 for j in range(m)] for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                total = img[i][j]
+                cnt = 1
+                for di, dj in d8:
+                    if 0 <= i + di < n and 0 <= j + dj < m:
+                        total += img[i + di][j + dj]
+                        cnt += 1
+                ans[i][j] = total // cnt
+        return ans
+
+
+def main():
+    img = [[100, 200, 100], [200, 50, 200], [100, 200, 100]]
+    print(Solution().imageSmoother(img))
+
+
+if __name__ == '__main__':
+    main()
