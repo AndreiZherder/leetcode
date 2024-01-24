c7d1db2b791fb8180302b86ae3eb08638fdbada3
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-01-24 09:31:34 +0300

    1457-pseudo-palindromic-paths-in-a-binary-tree2

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree2.py
@@ -0,0 +1,123 @@
+"""
+Given a binary tree where node values are digits from 1 to 9.
+A path in the binary tree is said to be pseudo-palindromic if at least one permutation of the node values in
+the path is a palindrome.
+
+Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+
+Example 1:
+Input: root = [2,3,1,3,1,null,1]
+Output: 2
+Explanation: The figure above represents the given binary tree.
+There are three paths going from the root node to leaf nodes: the red path [2,3,3], the green path [2,1,1],
+and the path [2,3,1]. Among these paths only red path and green path are pseudo-palindromic paths since the red path
+[2,3,3] can be rearranged in [3,2,3] (palindrome) and the green path [2,1,1] can be rearranged in [1,2,1] (palindrome).
+Example 2:
+Input: root = [2,1,1,1,3,null,null,null,null,null,1]
+Output: 1
+Explanation: The figure above represents the given binary tree.
+There are three paths going from the root node to leaf nodes:
+the green path [2,1,1], the path [2,1,3,1], and the path [2,1].
+Among these paths only the green path is pseudo-palindromic since [2,1,1] can be rearranged in [1,2,1] (palindrome).
+Example 3:
+
+Input: root = [9]
+Output: 1
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 10^5].
+1 <= Node.val <= 9
+"""
+from collections import Counter
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        def dfs(root: TreeNode, cur) -> int:
+            cur[root.val] += 1
+            if not root.left and not root.right:
+                ans = int(sum(v % 2 == 1 for v in cur.values()) <= 1)
+            else:
+                ans = 0
+                if root.left:
+                    ans += dfs(root.left, cur)
+                if root.right:
+                    ans += dfs(root.right, cur)
+            cur[root.val] -= 1
+            return ans
+        return dfs(root, Counter())
+
+
+def main():
+    vals = [2, 3, 1, 3, 1, None, 1]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().pseudoPalindromicPaths(root))
+
+
+if __name__ == '__main__':
+    main()
