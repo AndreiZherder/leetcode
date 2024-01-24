a19dbb180eb075c02c7fc97ee44f6daa3b926100
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-01-24 13:57:27 +0300

    1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k

---
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.py
@@ -0,0 +1,59 @@
+"""
+Given an integer k, return the minimum number of Fibonacci numbers whose sum is equal to k.
+The same Fibonacci number can be used multiple times.
+
+The Fibonacci numbers are defined as:
+
+F1 = 1
+F2 = 1
+Fn = Fn-1 + Fn-2 for n > 2.
+It is guaranteed that for the given constraints we can always find such Fibonacci numbers that sum up to k.
+
+
+Example 1:
+
+Input: k = 7
+Output: 2
+Explanation: The Fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, ...
+For k = 7 we can use 2 + 5 = 7.
+Example 2:
+
+Input: k = 10
+Output: 2
+Explanation: For k = 10 we can use 2 + 8 = 10.
+Example 3:
+
+Input: k = 19
+Output: 3
+Explanation: For k = 19 we can use 1 + 5 + 13 = 19.
+
+
+Constraints:
+
+1 <= k <= 109
+
+"""
+from bisect import bisect_right
+from typing import List
+
+
+class Solution:
+    def findMinFibonacciNumbers(self, k: int) -> int:
+        nums = [1, 1]
+        while nums[-1] < k:
+            nums.append(nums[-2] + nums[-1])
+        ans = 0
+        while k:
+            k -= nums[bisect_right(nums, k) - 1]
+            ans += 1
+        return ans
+
+
+
+def main():
+    k = 19
+    print(Solution().findMinFibonacciNumbers(k))
+
+
+if __name__ == '__main__':
+    main()
