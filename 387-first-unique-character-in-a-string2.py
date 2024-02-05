1fc5cd251f4550795a7b9b6d894d25ea66abda3d
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-02-05 13:25:28 +0300

    387-first-unique-character-in-a-string2

---
+++ 387-first-unique-character-in-a-string2.py
@@ -0,0 +1,43 @@
+"""
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+"""
+from collections import Counter
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counter = Counter(s)
+        for i, c in enumerate(s):
+            if counter[c] == 1:
+                return i
+        return -1
+
+
+def main():
+    s = "loveleetcode"
+    print(Solution().firstUniqChar(s))
+
+
+if __name__ == '__main__':
+    main()
