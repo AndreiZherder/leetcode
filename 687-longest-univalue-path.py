890824b3f87a8b917a92f0e06cbf0665500a8cf2
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-02-13 17:11:10 +0300

    687-longest-univalue-path

---
+++ 687-longest-univalue-path.py
@@ -0,0 +1,121 @@
+"""
+Given the root of a binary tree, return the length of the longest path, where each node in the path has the same value.
+This path may or may not pass through the root.
+
+The length of the path between two nodes is represented by the number of edges between them.
+
+
+
+Example 1:
+Input: root = [5,4,5,1,1,null,5]
+Output: 2
+Explanation: The shown image shows that the longest path of the same value (i.e. 5).
+Example 2:
+Input: root = [1,4,5,4,4,null,5]
+Output: 2
+Explanation: The shown image shows that the longest path of the same value (i.e. 4).
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-1000 <= Node.val <= 1000
+The depth of the tree will not exceed 1000.
+"""
+from typing import Optional, List, Tuple
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        def dfs(root: TreeNode) -> (int, int):
+            depth_left = 0
+            depth_right = 0
+            ans_left = 0
+            ans_right = 0
+            if root.left:
+                depth_left, ans_left = dfs(root.left)
+                if root.left.val != root.val:
+                    depth_left = 0
+            if root.right:
+                depth_right, ans_right = dfs(root.right)
+                if root.right.val != root.val:
+                    depth_right = 0
+            depth = 1 + max(depth_left, depth_right)
+            return depth, max(ans_left, ans_right, depth_left + depth_right + 1)
+
+        if not root:
+            return 0
+        return dfs(root)[1] - 1
+
+
+
+def main():
+    vals = [1, 4, 5, 4, 4, None, 5]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().longestUnivaluePath(root))
+
+
+if __name__ == '__main__':
+    main()
