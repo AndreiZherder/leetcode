f0dfa0ea31646c5446247485468895e9f2ea756d
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-03 15:06:58 +0300

    2094-finding-3-digit-even-numbers

---
+++ 2094-finding-3-digit-even-numbers.py
@@ -0,0 +1,65 @@
+"""
+You are given an integer array digits, where each element is a digit. The array may contain duplicates.
+
+You need to find all the unique integers that follow the given requirements:
+
+The integer consists of the concatenation of three elements from digits in any arbitrary order.
+The integer does not have leading zeros.
+The integer is even.
+For example, if the given digits were [1, 2, 3], integers 132 and 312 follow the requirements.
+
+Return a sorted array of the unique integers.
+
+
+
+Example 1:
+
+Input: digits = [2,1,3,0]
+Output: [102,120,130,132,210,230,302,310,312,320]
+Explanation: All the possible integers that follow the requirements are in the output array.
+Notice that there are no odd integers or integers with leading zeros.
+Example 2:
+
+Input: digits = [2,2,8,8,2]
+Output: [222,228,282,288,822,828,882]
+Explanation: The same digit can be used as many times as it appears in digits.
+In this example, the digit 8 is used twice each time in 288, 828, and 882.
+Example 3:
+
+Input: digits = [3,7,5]
+Output: []
+Explanation: No even integers can be formed using the given digits.
+
+
+Constraints:
+
+3 <= digits.length <= 100
+0 <= digits[i] <= 9
+
+"""
+from typing import List
+
+
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        s = set()
+        for i in range(n):
+            if digits[i] != 0:
+                for j in range(n):
+                    if j != i:
+                        for k in range(n):
+                            if k != i and k != j:
+                                x = 100 * digits[i] + 10 * digits[j] + digits[k]
+                                if x % 2 == 0:
+                                    s.add(x)
+        return sorted(s)
+
+
+def main():
+    digits = [2, 1, 3, 0]
+    print(Solution().findEvenNumbers(digits))
+
+
+if __name__ == '__main__':
+    main()
