1b960f50d991fbbf2323551a0acdac94ed8da727
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-04 14:48:49 +0300

    1346-check-if-n-and-its-double-exist

---
+++ 1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,53 @@
+"""
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+
+
+Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+Example 2:
+
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+
+
+Constraints:
+
+2 <= arr.length <= 500
+-103 <= arr[i] <= 103
+
+"""
+from typing import List
+
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        s = set()
+        for num in arr:
+            if 2 * num in s:
+                return True
+            else:
+                s.add(num)
+        s = set()
+        for num in arr[::-1]:
+            if 2 * num in s:
+                return True
+            else:
+                s.add(num)
+        return False
+
+
+def main():
+    arr = [10, 2, 5, 3]
+    print(Solution().checkIfExist(arr))
+
+
+if __name__ == '__main__':
+    main()
