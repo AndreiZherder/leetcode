0586f1059ad94fea53571ed995bd0ce506839677
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-12 14:48:12 +0300

    1028-recover-a-tree-from-preorder-traversal

---
+++ 1028-recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,117 @@
+"""
+We run a preorder depth-first search (DFS) on the root of a binary tree.
+
+At each node in this traversal, we output D dashes (where D is the depth of this node),
+then we output the value of this node.  If the depth of a node is D, the depth of its immediate child is D + 1.
+ The depth of the root node is 0.
+
+If a node has only one child, that child is guaranteed to be the left child.
+
+Given the output traversal of this traversal, recover the tree and return its root.
+
+
+
+Example 1:
+Input: traversal = "1-2--3--4-5--6--7"
+Output: [1,2,5,3,4,6,7]
+Example 2:
+Input: traversal = "1-2--3---4-5--6---7"
+Output: [1,2,5,3,null,6,null,4,null,7]
+Example 3:
+Input: traversal = "1-401--349---90--88"
+Output: [1,401,null,349,88,90]
+
+
+Constraints:
+
+The number of nodes in the original tree is in the range [1, 1000].
+1 <= Node.val <= 109
+"""
+from typing import Optional, List, Tuple
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        def dfs(d: int, s: str ) -> TreeNode:
+            print(d, s)
+            i = s.find('-' * d)
+            if i == -1:
+                return TreeNode(int(s))
+            else:
+                root = TreeNode(int(s[:i]))
+                j = s.find('-' * d, i + 1)
+                while j != -1 and (not s[j - 1].isdigit() or not s[j + d].isdigit()):
+                    j = s.find('-' * d, j + 1)
+                if j == -1:
+                    root.left = dfs(d + 1, s[i + d:])
+                else:
+                    root.left = dfs(d + 1, s[i + d:j])
+                    root.right = dfs(d + 1, s[j + d:])
+            return root
+        return dfs(1, traversal)
+
+
+
+def main():
+    traversal = "1-2--3--4-5--6--7"
+    print(Solution().recoverFromPreorder(traversal))
+
+
+if __name__ == '__main__':
+    main()
