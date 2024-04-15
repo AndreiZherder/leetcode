e34b089743b83141aa878a50c92eb5009b841cb8
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-15 23:09:00 +0300

    654-maximum-binary-tree

---
+++ 654-maximum-binary-tree.py
@@ -0,0 +1,138 @@
+"""
+You are given an integer array nums with no duplicates. A maximum binary tree can be built recursively from nums
+using the following algorithm:
+
+Create a root node whose value is the maximum value in nums.
+Recursively build the left subtree on the subarray prefix to the left of the maximum value.
+Recursively build the right subtree on the subarray suffix to the right of the maximum value.
+Return the maximum binary tree built from nums.
+
+
+
+Example 1:
+Input: nums = [3,2,1,6,0,5]
+Output: [6,3,5,null,2,0,null,null,1]
+Explanation: The recursive calls are as follow:
+- The largest value in [3,2,1,6,0,5] is 6. Left prefix is [3,2,1] and right suffix is [0,5].
+    - The largest value in [3,2,1] is 3. Left prefix is [] and right suffix is [2,1].
+        - Empty array, so no child.
+        - The largest value in [2,1] is 2. Left prefix is [] and right suffix is [1].
+            - Empty array, so no child.
+            - Only one element, so child is a node with value 1.
+    - The largest value in [0,5] is 5. Left prefix is [0] and right suffix is [].
+        - Only one element, so child is a node with value 0.
+        - Empty array, so no child.
+Example 2:
+Input: nums = [3,2,1]
+Output: [3,null,2,null,1]
+
+
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 1000
+All integers in nums are unique.
+"""
+from collections import deque, defaultdict
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class RangeQuery:
+    """
+    Range queries on a static array
+    """
+
+    def __init__(self, data, func=min):
+        self.func = func
+        self._data = _data = [list(data)]
+        i, n = 1, len(_data[0])
+        while 2 * i <= n:
+            prev = _data[-1]
+            _data.append([func(prev[j], prev[j + i]) for j in range(n - 2 * i + 1)])
+            i <<= 1
+
+    def query(self, start, stop):
+        """func of data[start, stop)"""
+        depth = (stop - start).bit_length() - 1
+        return self.func(self._data[depth][start], self._data[depth][stop - (1 << depth)])
+
+    def __getitem__(self, idx):
+        return self._data[0][idx]
+
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        def dfs(left: int, right: int) -> TreeNode:
+            if left > right:
+                return
+            i = rq.query(left, right + 1)
+            return TreeNode(nums[i], dfs(left, i - 1), dfs(i + 1, right))
+
+        n = len(nums)
+        rq = RangeQuery(list(range(n)), func=lambda i, j: max(i, j, key=nums.__getitem__))
+        return dfs(0, n - 1)
+
+
+def main():
+    nums = [3, 2, 1, 6, 0, 5]
+    print(Solution().constructMaximumBinaryTree(nums))
+
+
+if __name__ == '__main__':
+    main()
