c4212ed7a69cd1b98ebfc41aeacf3029082b8c89
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-16 23:14:43 +0300

    1382-balance-a-binary-search-tree

---
+++ 1382-balance-a-binary-search-tree.py
@@ -0,0 +1,113 @@
+"""
+Given the root of a binary search tree, return a balanced binary search tree with the same node values.
+If there is more than one answer, return any of them.
+
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+
+
+
+Example 1:Input: root = [1,null,2,null,3,null,4,null,null]
+Output: [2,1,3,null,null,null,4]
+Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct.
+Example 2:
+Input: root = [2,1,3]
+Output: [2,1,3]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+1 <= Node.val <= 105
+
+"""
+from collections import deque, defaultdict
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        def inorder(root: TreeNode) -> int:
+            if root.left:
+                yield from inorder(root.left)
+            yield root.val
+            if root.right:
+                yield from inorder(root.right)
+        def dfs(left: int, right: int) -> Optional[TreeNode]:
+            if left > right:
+                return
+            mid = left + (right - left) // 2
+            return TreeNode(arr[mid], dfs(left, mid - 1), dfs(mid + 1, right))
+        arr = list(inorder(root))
+        return dfs(0, len(arr) - 1)
+
+
+def main():
+    vals = [2, 1, 3]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().balanceBST(root))
+
+
+if __name__ == '__main__':
+    main()
