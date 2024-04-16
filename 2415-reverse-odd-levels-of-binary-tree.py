716c6421ef9a2f401c03d3a9ff822dad1971b6fc
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-04-16 21:59:50 +0300

    2415-reverse-odd-levels-of-binary-tree

---
+++ 2415-reverse-odd-levels-of-binary-tree.py
@@ -0,0 +1,139 @@
+"""
+Given the root of a perfect binary tree, reverse the node values at each odd level of the tree.
+
+For example, suppose the node values at level 3 are [2,1,3,4,7,11,29,18], then it should become [18,29,11,7,4,3,1,2].
+Return the root of the reversed tree.
+
+A binary tree is perfect if all parent nodes have two children and all leaves are on the same level.
+
+The level of a node is the number of edges along the path between it and the root node.
+
+
+
+Example 1:
+Input: root = [2,3,5,8,13,21,34]
+Output: [2,5,3,8,13,21,34]
+Explanation:
+The tree has only one odd level.
+The nodes at level 1 are 3, 5 respectively, which are reversed and become 5, 3.
+Example 2:
+Input: root = [7,13,11]
+Output: [7,11,13]
+Explanation:
+The nodes at level 1 are 13, 11, which are reversed and become 11, 13.
+Example 3:
+
+Input: root = [0,1,2,0,0,0,0,1,1,1,1,2,2,2,2]
+Output: [0,2,1,0,0,0,0,2,2,2,2,1,1,1,1]
+Explanation:
+The odd levels have non-zero values.
+The nodes at level 1 were 1, 2, and are 2, 1 after the reversal.
+The nodes at level 3 were 1, 1, 1, 1, 2, 2, 2, 2, and are 2, 2, 2, 2, 1, 1, 1, 1 after the reversal.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 214].
+0 <= Node.val <= 105
+root is a perfect binary tree.
+"""
+from collections import deque, defaultdict
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        q = deque()
+        q.append((0, root))
+        q1 = deque()
+        while q:
+            nodes = []
+            while q:
+                level, node = q.popleft()
+                if level % 2 == 1:
+                    nodes.append(node)
+                if node.left:
+                    q1.append((level + 1, node.left))
+                if node.right:
+                    q1.append((level + 1, node.right))
+            if nodes:
+                i = 0
+                j = len(nodes) - 1
+                while i < j:
+                    nodes[i].val, nodes[j].val = nodes[j].val, nodes[i].val
+                    i += 1
+                    j -= 1
+            q, q1 = q1, q
+        return root
+
+
+def main():
+    vals = [2, 3, 5, 8, 13, 21, 34]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().reverseOddLevels(root))
+
+
+if __name__ == '__main__':
+    main()
