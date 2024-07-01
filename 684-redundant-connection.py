1fe35791e608acfc9f707bbfd1ad5c494a01e352
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-07-01 21:18:32 +0300

    684-redundant-connection

---
+++ 684-redundant-connection.py
@@ -0,0 +1,77 @@
+"""
+In this problem, a tree is an undirected graph that is connected and has no cycles.
+
+You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+
+Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+
+
+
+Example 1:
+Input: edges = [[1,2],[1,3],[2,3]]
+Output: [2,3]
+Example 2:
+Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+Output: [1,4]
+
+
+Constraints:
+
+n == edges.length
+3 <= n <= 1000
+edges[i].length == 2
+1 <= ai < bi <= edges.length
+ai != bi
+There are no repeated edges.
+The given graph is connected.
+
+"""
+from typing import List
+
+
+class DSU:
+    def __init__(self, n: int):
+        self.n = n
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, i: int) -> int:
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i: int, j: int):
+        id1 = self.find(i)
+        id2 = self.find(j)
+        if id1 == id2:
+            return
+        if self.rank[id1] > self.rank[id2]:
+            self.parent[id2] = id1
+        else:
+            self.parent[id1] = id2
+            if self.rank[id1] == self.rank[id2]:
+                self.rank[id2] += 1
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = 1000
+        dsu = DSU(n)
+        ans = []
+        for v, u in edges:
+            v -= 1
+            u -= 1
+            if dsu.find(v) == dsu.find(u):
+                ans = [v + 1, u + 1]
+            else:
+                dsu.union(v, u)
+        return ans
+
+
+def main():
+    edges = [[1, 2], [1, 3], [2, 3]]
+    print(Solution().findRedundantConnection(edges))
+
+
+if __name__ == '__main__':
+    main()
