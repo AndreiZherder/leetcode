16372f665187f81a89c7ef70e3a430aca371341b
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-07-16 09:13:17 +0300

    2096-step-by-step-directions-from-a-binary-tree-node-to-another

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,140 @@
+"""
+You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n.
+You are also given an integer startValue representing the value of the start node s,
+and a different integer destValue representing the value of the destination node t.
+
+Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path
+as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+
+'L' means to go from a node to its left child node.
+'R' means to go from a node to its right child node.
+'U' means to go from a node to its parent node.
+Return the step-by-step directions of the shortest path from node s to node t.
+
+
+
+Example 1:
+Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
+Output: "UURL"
+Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
+Example 2:
+Input: root = [2,1], startValue = 2, destValue = 1
+Output: "L"
+Explanation: The shortest path is: 2 → 1.
+
+
+Constraints:
+
+The number of nodes in the tree is n.
+2 <= n <= 105
+1 <= Node.val <= n
+All the values in the tree are unique.
+1 <= startValue, destValue <= n
+startValue != destValue
+"""
+from collections import defaultdict
+from typing import Optional, List, Tuple
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        def make_graph(root):
+            if root.left:
+                g[root.val].append((root.left.val, 'L'))
+                g[root.left.val].append((root.val, 'U'))
+                make_graph(root.left)
+            if root.right:
+                g[root.val].append((root.right.val, 'R'))
+                g[root.right.val].append((root.val, 'U'))
+                make_graph(root.right)
+
+        def backtrack(v: int, par: int, cur: List[str]) -> bool:
+            if v == destValue:
+                ans.append(cur[:])
+                return True
+            for u, direction in g[v]:
+                if u != par:
+                    cur.append(direction)
+                    if backtrack(u, v, cur):
+                        return True
+                    cur.pop()
+
+        g = defaultdict(list)
+        make_graph(root)
+        ans = []
+        backtrack(startValue, -1, [])
+        return ''.join(ans[0])
+
+
+def main():
+    vals = [5, 1, 2, 3, None, 6, 4]
+    startValue = 3
+    destValue = 6
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().getDirections(root, startValue, destValue))
+
+
+if __name__ == '__main__':
+    main()
