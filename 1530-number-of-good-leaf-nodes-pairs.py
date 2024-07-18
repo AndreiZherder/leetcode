806fba00010dd2be78ea7b5114258098500e4849
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-07-18 11:56:25 +0300

    1530-number-of-good-leaf-nodes-pairs

---
+++ 1530-number-of-good-leaf-nodes-pairs.py
@@ -0,0 +1,137 @@
+"""
+You are given the root of a binary tree and an integer distance. A pair of two different leaf nodes
+of a binary tree is said to be good if the length of the shortest path between them is less than or equal to distance.
+
+Return the number of good leaf node pairs in the tree.
+
+
+
+Example 1:
+Input: root = [1,2,3,null,4], distance = 3
+Output: 1
+Explanation: The leaf nodes of the tree are 3 and 4 and the length of the shortest path between them is 3.
+This is the only good pair.
+Example 2:
+Input: root = [1,2,3,4,5,6,7], distance = 3
+Output: 2
+Explanation: The good pairs are [4,5] and [6,7] with shortest path = 2. The pair [4,6]
+is not good because the length of ther shortest path between them is 4.
+Example 3:
+
+Input: root = [7,1,4,6,null,5,3,null,null,null,null,null,2], distance = 3
+Output: 1
+Explanation: The only good pair is [2,5].
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 210].
+1 <= Node.val <= 100
+1 <= distance <= 10
+"""
+from collections import defaultdict, deque
+from typing import Optional, List, Tuple
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        def dfs(root):
+            if root.left:
+                g[root].append(root.left)
+                g[root.left].append(root)
+                dfs(root.left)
+            if root.right:
+                g[root].append(root.right)
+                g[root.right].append(root)
+                dfs(root.right)
+            if not root.left and not root.right:
+                leaves.add(root)
+
+        g = defaultdict(list)
+        leaves = set()
+        dfs(root)
+        ans = 0
+        for v in leaves:
+            q = deque([(v, -1, 0)])
+            while q:
+                v, par, d = q.popleft()
+                for u in g[v]:
+                    if u != par:
+                        if u in leaves:
+                            ans += 1
+                        elif d + 2 <= distance:
+                            q.append((u, v, d + 1))
+        return ans // 2
+
+
+def main():
+    vals = [1, 1, 1]
+    distance = 2
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().countPairs(root, distance))
+
+
+if __name__ == '__main__':
+    main()
