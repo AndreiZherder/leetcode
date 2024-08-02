39cc4f97f73f1d37b16852bb529255ba6812ba4e
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-08-02 15:24:25 +0300

    2134-minimum-swaps-to-group-all-1s-together-ii

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -0,0 +1,68 @@
+"""
+A swap is defined as taking two distinct positions in an array and swapping the values in them.
+
+A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+
+Given a binary circular array nums, return the minimum number of swaps required to group all 1's present
+in the array together at any location.
+
+
+
+Example 1:
+
+Input: nums = [0,1,0,1,1,0,0]
+Output: 1
+Explanation: Here are a few of the ways to group all the 1's together:
+[0,0,1,1,1,0,0] using 1 swap.
+[0,1,1,1,0,0,0] using 1 swap.
+[1,1,0,0,0,0,1] using 2 swaps (using the circular property of the array).
+There is no way to group all 1's together with 0 swaps.
+Thus, the minimum number of swaps required is 1.
+Example 2:
+
+Input: nums = [0,1,1,1,0,0,1,1,0]
+Output: 2
+Explanation: Here are a few of the ways to group all the 1's together:
+[1,1,1,0,0,0,0,1,1] using 2 swaps (using the circular property of the array).
+[1,1,1,1,1,0,0,0,0] using 2 swaps.
+There is no way to group all 1's together with 0 or 1 swaps.
+Thus, the minimum number of swaps required is 2.
+Example 3:
+
+Input: nums = [1,1,0,0,1]
+Output: 0
+Explanation: All the 1's are already grouped together due to the circular property of the array.
+Thus, the minimum number of swaps required is 0.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+
+"""
+from typing import List
+
+
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        n = len(nums)
+        target = nums.count(1)
+        if target < 2:
+            return 0
+        nums *= 2
+        cur = nums[:target].count(0)
+        ans = cur
+        for i in range(target, 2 * n):
+            cur += (nums[i] == 0) - (nums[i - target] == 0)
+            ans = min(ans, cur)
+        return ans
+
+
+def main():
+    nums = [1, 1, 0, 0, 1]
+    print(Solution().minSwaps(nums))
+
+
+if __name__ == '__main__':
+    main()
