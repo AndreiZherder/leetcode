b99c8bdec38a6900b2620738ad429d5fa316bead
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-10-26 12:00:03 +0300

    2458-height-of-binary-tree-after-subtree-removal-queries

---
+++ 2458-height-of-binary-tree-after-subtree-removal-queries.py
@@ -0,0 +1,157 @@
+"""
+You are given the root of a binary tree with n nodes. Each node is assigned a unique value from 1 to n.
+You are also given an array queries of size m.
+
+You have to perform m independent queries on the tree where in the ith query you do the following:
+
+Remove the subtree rooted at the node with the value queries[i] from the tree.
+It is guaranteed that queries[i] will not be equal to the value of the root.
+Return an array answer of size m where answer[i] is the height of the tree after performing the ith query.
+
+Note:
+
+The queries are independent, so the tree returns to its initial state after each query.
+The height of a tree is the number of edges in the longest simple path from the root to some node in the tree.
+
+
+Example 1:
+Input: root = [1,3,4,2,null,6,5,null,null,null,null,null,7], queries = [4]
+Output: [2]
+Explanation: The diagram above shows the tree after removing the subtree rooted at node with value 4.
+The height of the tree is 2 (The path 1 -> 3 -> 2).
+Example 2:
+Input: root = [5,8,9,2,1,3,7,4,6], queries = [3,2,4,8]
+Output: [3,2,3,2]
+Explanation: We have the following queries:
+- Removing the subtree rooted at node with value 3. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 4).
+- Removing the subtree rooted at node with value 2. The height of the tree becomes 2 (The path 5 -> 8 -> 1).
+- Removing the subtree rooted at node with value 4. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 6).
+- Removing the subtree rooted at node with value 8. The height of the tree becomes 2 (The path 5 -> 9 -> 3).
+
+
+Constraints:
+
+The number of nodes in the tree is n.
+2 <= n <= 105
+1 <= Node.val <= n
+All the values in the tree are unique.
+m == queries.length
+1 <= m <= min(n, 104)
+1 <= queries[i] <= n
+queries[i] != root.val
+"""
+from collections import deque, defaultdict
+from typing import Optional, List
+from sortedcontainers import SortedList
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        lines, *_ = self.__display_aux()
+        return '\n'.join(lines)
+
+    def __display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if not self.right and not self.left:
+            line = f"{self.val}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if not self.right:
+            lines, n, p, x = self.left.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if not self.left:
+            lines, n, p, x = self.right.__display_aux()
+            s = f"{self.val}"
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.__display_aux()
+        right, m, q, y = self.right.__display_aux()
+        s = f"{self.val}"
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class Solution:
+    def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
+        def dfs(v: TreeNode, level: int) -> int:
+            node_level[v.val] = level
+            if not v.left and not v.right:
+                level_nodes[level].add((0, v.val))
+                node_height[v.val] = 0
+                return 0
+            left_height = 0
+            right_height = 0
+            if v.left:
+                left_height = dfs(v.left, level + 1)
+            if v.right:
+                right_height = dfs(v.right, level + 1)
+            height = max(left_height, right_height) + 1
+            level_nodes[level].add((height, v.val))
+            node_height[v.val] = height
+            return height
+
+        node_level = dict()
+        node_height = dict()
+        level_nodes = defaultdict(lambda: SortedList())
+        dfs(root, 0)
+        root_height = node_height[root.val]
+        ans = []
+        for v in queries:
+            level = node_level[v]
+            height = node_height[v]
+            if len(level_nodes[level]) == 1:
+                ans.append(root_height - height - 1)
+            else:
+                if level_nodes[level][-1][1] != v:
+                    ans.append(root_height)
+                else:
+                    new_height = level_nodes[level][-2][0]
+                    ans.append(root_height - (height - new_height))
+        return ans
+
+
+def main():
+    vals = [5, 8, 9, 2, 1, 3, 7, 4, 6]
+    queries = [3, 2, 4, 8]
+    nodes = [TreeNode(val) if val is not None else None for val in vals]
+    root = nodes[0] if nodes else None
+    for i, node in enumerate(nodes):
+        if node:
+            node.left = nodes[i * 2 + 1] if i * 2 + 1 < len(nodes) else None
+            node.right = nodes[i * 2 + 2] if i * 2 + 2 < len(nodes) else None
+    print(root)
+    print(Solution().treeQueries(root, queries))
+
+
+if __name__ == '__main__':
+    main()
