2b389cc96bbe506cea30ab0b8d7329a2b5a25dc5
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2024-12-14 10:44:20 +0300

    2762-continuous-subarrays

---
+++ 2762-continuous-subarrays.py
@@ -0,0 +1,89 @@
+"""
+You are given a 0-indexed integer array nums. A subarray of nums is called continuous if:
+
+Let i, i + 1, ..., j be the indices in the subarray. Then, for each pair of indices i <= i1, i2 <= j,
+0 <= |nums[i1] - nums[i2]| <= 2.
+Return the total number of continuous subarrays.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+
+Example 1:
+
+Input: nums = [5,4,2,4]
+Output: 8
+Explanation:
+Continuous subarray of size 1: [5], [4], [2], [4].
+Continuous subarray of size 2: [5,4], [4,2], [2,4].
+Continuous subarray of size 3: [4,2,4].
+Thereare no subarrys of size 4.
+Total continuous subarrays = 4 + 3 + 1 = 8.
+It can be shown that there are no more continuous subarrays.
+
+
+Example 2:
+
+Input: nums = [1,2,3]
+Output: 6
+Explanation:
+Continuous subarray of size 1: [1], [2], [3].
+Continuous subarray of size 2: [1,2], [2,3].
+Continuous subarray of size 3: [1,2,3].
+Total continuous subarrays = 3 + 2 + 1 = 6.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+
+"""
+from typing import List
+
+
+# https://github.com/cheran-senthil/PyRival/blob/master/pyrival/data_structures/RangeQuery.py
+class RangeQuery:
+    """
+    Range queries on a static array
+    """
+
+    def __init__(self, data, func=min):
+        self.func = func
+        self._data = _data = [list(data)]
+        i, n = 1, len(_data[0])
+        while 2 * i <= n:
+            prev = _data[-1]
+            _data.append([func(prev[j], prev[j + i]) for j in range(n - 2 * i + 1)])
+            i <<= 1
+
+    def query(self, start, stop):
+        """func of data[start, stop)"""
+        depth = (stop - start).bit_length() - 1
+        return self.func(self._data[depth][start], self._data[depth][stop - (1 << depth)])
+
+    def __getitem__(self, idx):
+        return self._data[0][idx]
+
+
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        mx = RangeQuery(nums, func=max)
+        mn = RangeQuery(nums, func=min)
+        ans = 0
+        j = 0
+        for i in range(n):
+            while mx.query(j, i + 1) - mn.query(j, i + 1) > 2:
+                j += 1
+            ans += i - j + 1
+        return ans
+
+
+def main():
+    nums = [5, 4, 2, 4]
+    print(Solution().continuousSubarrays(nums))
+
+
+if __name__ == '__main__':
+    main()
