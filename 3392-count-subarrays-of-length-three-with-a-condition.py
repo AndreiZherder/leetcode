12acee30d8242162fa64637dcbc623ee59ed85e9
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2025-04-27 10:01:33 +0300

    3392-count-subarrays-of-length-three-with-a-condition

---
+++ 3392-count-subarrays-of-length-three-with-a-condition.py
@@ -0,0 +1,55 @@
+"""
+Given an integer array nums, return the number of subarrays of length 3
+such that the sum of the first and third numbers equals exactly half of the second number.
+
+
+
+Example 1:
+
+Input: nums = [1,2,1,4,1]
+
+Output: 1
+
+Explanation:
+
+Only the subarray [1,4,1] contains exactly 3 elements where the sum of the
+first and third numbers equals half the middle number.
+
+Example 2:
+
+Input: nums = [1,1,1]
+
+Output: 0
+
+Explanation:
+
+[1,1,1] is the only subarray of length 3. However, its first and third numbers do not add to half the middle number.
+
+
+
+Constraints:
+
+3 <= nums.length <= 100
+-100 <= nums[i] <= 100
+
+"""
+from typing import List
+
+
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = 0
+        for i in range(2, n):
+            if nums[i - 1] % 2 == 0 and nums[i] + nums[i - 2] == nums[i - 1] // 2:
+                ans += 1
+        return ans
+
+
+def main():
+    nums = [1, 2, 1, 4, 1]
+    print(Solution().countSubarrays(nums))
+
+
+if __name__ == '__main__':
+    main()
